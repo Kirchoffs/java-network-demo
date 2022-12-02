0c615ce9f9557a91424ec289a652939fe43157b4
Yonghao Shu <123shuyonghao@gmail.com>
2022-12-01 22:03:17 -0800

    First commit

---
+++ src/main/java/org/syh/demo/netty/simpledemo/server/NettyServerHandler.java
@@ -0,0 +1,31 @@
+package org.syh.demo.netty.simpledemo.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("Received from client: " + buf.toString(CharsetUtil.UTF_8));
+        System.out.println("Client address: " + ctx.channel().remoteAddress());
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Hi client", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        Channel channel = ctx.channel();
+        if (channel.isActive()) {
+            ctx.close();
+        }
+    }
+}
